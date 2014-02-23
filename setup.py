8cb5789a1370ebeaaaa0a63357d6096dc6b53d79
Jeremy Slade <jeremy@jkslade.net>
2014-02-22 22:33:52 -0700

    0.2.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='monque',
-      version='0.2.3',
+      version='0.2.4',
       description='Persistent task queue built on top of MongoDB',
       url='http://github.com/jslade/python-monque',
       author='Jeremy Slade',
